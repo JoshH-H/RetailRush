177275d771d78be70fc6d1d5df5f59a0bd41cb8f
Evaldas Matutis <em6450z@gre.ac.uk>
2020-10-19 13:17:59 +0100

    Walking Boy

---
+++ UnityWork/Testing for Retail Rush/Assets/Sun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, 30f * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
