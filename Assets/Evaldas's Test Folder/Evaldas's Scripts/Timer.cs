ad938a8a8c52f011998408ea92fb9c05607c7f3a
Evaldas Matutis <em6450z@gre.ac.uk>
2021-02-09 15:44:05 +0000

    Uploading All the scripts I made

---
+++ Assets/Evaldas's Test Folder/Evaldas's Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float CurrentTime = 0f;
+    public float StartTime = 900f;
+
+    public GameObject ShiftsResults;
+
+    //public Text TimerO;
+
+    int Min;
+    int Sec;
+
+    void Start()
+    {
+        ShiftsResults.SetActive(false);
+        CurrentTime = StartTime;
+    }
+    void Update()
+    {
+        
+        CurrentTime -= 1 * Time.deltaTime;
+        //Min = Mathf.FloorToInt(CurrentTime / 60);
+        //Sec = Mathf.FloorToInt(CurrentTime % 60);
+        //TimerO.text = Min.ToString("00") + ":" + Sec.ToString("00");
+        if (CurrentTime <= 0 || Input.GetKeyDown("space"))
+        {
+            CurrentTime = 0;
+            ShiftsResults.SetActive(true);
+        }
+    }
+}
