4f3f67842818a533592ef55201ded51fa6ade8e0
Josh <j.hallhems@gmail.com>
2021-02-10 12:12:13 +0000

    Beta Dialogue Added, not tuned yet

---
+++ Assets/Josh's Scripts/AnswerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerScript : MonoBehaviour
+{
+    [SerializeField] public bool isCorrect = false;
+    [SerializeField] QuestionManager questionManager;
+
+    public void Answer()
+    {
+        if (isCorrect)
+        {
+            Debug.Log("That's right");
+            questionManager.correct();
+        }
+        else
+        {
+            Debug.Log("That's wrong");
+            questionManager.correct();
+        }
+    }
+}
---
+++ Assets/Josh's Scripts/QuestionManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionManager : MonoBehaviour
+{
+    [Header("Customize Questions")]
+    [SerializeField] public List<QnA> QuestionsNAnswers;
+    [SerializeField] public GameObject[] options;
+    [SerializeField] public int currentQuestion;
+
+    [Header("Link Question Text")]
+    [SerializeField] public Text QuestionTxt;
+
+    [Header("Pannel UI")]
+    [SerializeField] GameObject pannelUI;
+
+    private void Start()
+    {
+        generateQuestion();
+ 
+    }
+
+    public void correct()
+    {
+        QuestionsNAnswers.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    void SetAnswers()
+    {
+        for(int i = 0; i< options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QuestionsNAnswers[currentQuestion].Answers[i];
+
+            if(QuestionsNAnswers[currentQuestion].CorrectAnswer == i + 1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+        }
+    }
+
+    void generateQuestion()
+    {
+        if(QuestionsNAnswers.Count > 0) 
+        {
+
+            //currentQuestion =Random.Range (0, QuestionsNAnswers.Count);
+
+            QuestionTxt.text = QuestionsNAnswers[currentQuestion].Question;
+            SetAnswers();
+        }
+        else
+        {
+            Debug.Log("Complete");
+            pannelUI.SetActive(false);
+        }
+
+    }
+}
