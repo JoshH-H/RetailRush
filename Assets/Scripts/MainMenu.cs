15ff3a61022808d5d0765766c9694b905251f693
Leena <leena.jarvenpaa@windowslive.com>
2021-03-13 21:16:21 +0000

    Sounds for main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    //Leena's script to match menu to sounds
+    [SerializeField] private GameObject chooseRole;
+
+    public void OpenMenu()
+    {
+        StartCoroutine(WaitBeforeOpen());
+    }
+
+    private IEnumerator WaitBeforeOpen()
+    {
+        yield return new WaitForSeconds(0.3f);
+        chooseRole.SetActive(true);
+    }
+}
